054b1b2b5393451389581d1d4cf34e1836802655
gharrsha <guduruharrsha@gmail.com>
2026-03-29 22:11:00 +0530

    Welcome to the world of Typescript

---
+++ Implement/1.js
@@ -0,0 +1,10 @@
+var typescriptBasic = /** @class */ (function () {
+    function typescriptBasic() {
+    }
+    typescriptBasic.prototype.greet = function () {
+        return "Welcome to Typescript, All the best Harrsha";
+    };
+    return typescriptBasic;
+}());
+var ts = new typescriptBasic();
+console.log(ts.greet());
