2e7ad23e26f4ce8892ece9590fc06bd477318d70
gharrsha <guduruharrsha@gmail.com>
2026-03-29 22:34:56 +0530

    Prepare Well

---
+++ Implement/2.js
@@ -0,0 +1,10 @@
+var typescriptBasic = /** @class */ (function () {
+    function typescriptBasic() {
+    }
+    typescriptBasic.prototype.greet = function () {
+        return "Welcome to Typescript, All the best for the exam, Harrsha";
+    };
+    return typescriptBasic;
+}());
+var ts = new typescriptBasic();
+console.log(ts.greet());
---
+++ Implement/2.ts
@@ -0,0 +1,11 @@
+class typescriptBasic{
+    constructor(){
+
+    }
+    greet(): string{
+        return "Welcome to Typescript, All the best for the exam, Harrsha"
+    }
+}
+
+const ts = new typescriptBasic();
+console.log(ts.greet());
